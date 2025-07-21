fdf30d4cde2dc6832c31ef67a7474c247472a453
Ayush-Kumar2005 <ayush26052005hjp@gmail.com>
2025-07-22 02:12:46 +0530

    Production bugs fixed

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,22 +1,33 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import {addNowPlayingMovies} from "../utils/movieSlice"
-import { API_OPTIONS } from "../utils/constants";    
-    
-const useNowPlayingMovies = () =>{
-        // Fetch Data from TMDB API and update store.
+import { addNowPlayingMovies } from "../utils/movieSlice";
+import { API_OPTIONS } from "../utils/constants";
+
+const useNowPlayingMovies = () => {
+    // Fetch Data from TMDB API and update store.
     const dispatch = useDispatch();
 
-    const getNowPlayingMovies = async () =>{
-        const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1',API_OPTIONS);
-        const json = await data.json();
-        // console.log(json.results);
-        dispatch(addNowPlayingMovies(json.results))
+    const getNowPlayingMovies = async () => {
+        try {
+            const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', API_OPTIONS);
+
+            if (!data.ok) {
+                throw new Error("Failed to fetch Now Playing movies");
+            }
+
+            const json = await data.json();
+            // console.log(json.results);
+            if (json?.results?.length) {
+                dispatch(addNowPlayingMovies(json.results));
+            }
+        } catch (error) {
+            console.error("Error fetching now playing movies:", error);
+        }
     };
 
-    useEffect(()=>{
+    useEffect(() => {
         getNowPlayingMovies();
-    },[dispatch]);
+    }, [dispatch]);
 
     // This API is called twice every times due to the "React.StrictMode" that is written in "index.js" file.
     // React does extra rendering of your components to check some inconsistencies between your calls.
